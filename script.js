23a9d29fccb4b07623de4efa70263c64602a438c
mvttn <85101143+mvttn@users.noreply.github.com>
2024-01-17 18:11:51 +1100

    Create functions for each operation

---
+++ script.js
@@ -0,0 +1,23 @@
+let INPUT1;
+let INPUT2;
+let OPERATOR;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  return add(num1, num2);
+}
